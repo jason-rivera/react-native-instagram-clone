8bd7d3d0ffe1c1984588b41c2520c9c7255106c3
ZORDON-PC\Jason <jp_rivera_@hotmail.com>
2023-06-27 12:09:38 -0700

    updated logo on home page

--- components/home/HomeHeader.jsx
+++ components/home/HomeHeader.jsx
@@ -1,15 +1,30 @@
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
 
 import styles from './HomeHeader.style';
 
 const HomeHeader = () => {
   return (
     <View>
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-        <Text>Instagram</Text>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          paddingHorizontal: 10,
+        }}
+      >
+        <View style={{ height: 40, width: 100 }}>
+          <Image
+            style={{ flex: 1 }}
+            source={{
+              uri: 'https://1000logos.net/wp-content/uploads/2017/02/Logo-Instagram-1-500x281.png',
+            }}
+          />
+        </View>
         <View
           style={{
             flexDirection: 'row',
+            gap: 10,
+            fontSize: 20,
           }}
         >
           <Text>♥</Text>
