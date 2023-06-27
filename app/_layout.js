df3f5afc646732918f29fb4e25622e3702e281da
ZORDON-PC\Jason <jp_rivera_@hotmail.com>
2023-06-27 11:06:07 -0700

    add partial search page

--- app/_layout.js
+++ app/_layout.js
@@ -37,7 +37,7 @@ const Layout = () => {
             }}
           />
           <Tabs.Screen
-            name='search'
+            name='search/search'
             options={{
               tabBarIcon: () => <Icon name='search' size={20} />,
             }}
---
+++ app/search/search.js
@@ -0,0 +1,41 @@
+import {
+  View,
+  Text,
+  SafeAreaView,
+  TouchableWithoutFeedback,
+  TextInput,
+  Keyboard,
+} from 'react-native';
+
+const Search = () => {
+  const handleTouchOutside = () => {
+    Keyboard.dismiss();
+  };
+
+  return (
+    // <SafeAreaView>
+    <TouchableWithoutFeedback onPress={handleTouchOutside}>
+      <View
+        style={{ backgroundColor: 'white', flex: 1 }}
+        onPress={handleTouchOutside}
+      >
+        <TextInput
+          style={{
+            borderWidth: 1,
+            borderColor: '#f0f0f0',
+            backgroundColor: '#f0f0f0',
+            margin: 5,
+            height: 40,
+            borderRadius: 5,
+            paddingHorizontal: 10,
+            fontSize: 16,
+          }}
+          placeholder='🔍 Search'
+        />
+      </View>
+    </TouchableWithoutFeedback>
+    // </SafeAreaView>
+  );
+};
+
+export default Search;
