7045ba5be1ea1d008dafa4f89057aba1a3300fd6
ZORDON-PC\Jason <jp_rivera_@hotmail.com>
2023-06-27 12:14:30 -0700

    fixed virtualizedlists warning

--- app/home/home.js
+++ app/home/home.js
@@ -39,62 +39,64 @@ const posts = [
 
 const Home = () => {
   return (
-    <ScrollView style={{ backgroundColor: 'white' }}>
-      <HomeHeader />
-      <FlatList
-        data={profiles}
-        renderItem={({ item }) => (
-          <View>
-            <Image
-              style={styles.userIcon}
-              source={images.goku_god}
-              resizeMode='contain'
-            />
-            <Text style={styles.userName}>{item}</Text>
-          </View>
-        )}
-        keyExtractor={(item) => item}
-        horizontal
-      />
-      <FlatList
-        data={posts}
-        renderItem={({ item }) => (
-          <View>
-            <Image
-              style={{ width: '100%', height: 500 }}
-              source={{
-                uri: 'https://i.pinimg.com/originals/30/e7/cf/30e7cfb75da311ffd3a658ab798ec7ab.jpg',
-              }}
-            />
+    <FlatList
+      ListHeaderComponent={
+        <>
+          <HomeHeader />
+          <FlatList
+            data={profiles}
+            renderItem={({ item }) => (
+              <View>
+                <Image
+                  style={styles.userIcon}
+                  source={images.goku_god}
+                  resizeMode='contain'
+                />
+                <Text style={styles.userName}>{item}</Text>
+              </View>
+            )}
+            keyExtractor={(item) => item}
+            horizontal
+          />
+        </>
+      }
+      data={posts}
+      renderItem={({ item }) => (
+        <View>
+          <Image
+            style={{ width: '100%', height: 500 }}
+            source={{
+              uri: 'https://i.pinimg.com/originals/30/e7/cf/30e7cfb75da311ffd3a658ab798ec7ab.jpg',
+            }}
+          />
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+            }}
+          >
             <View
               style={{
                 flexDirection: 'row',
-                justifyContent: 'space-between',
               }}
             >
-              <View
-                style={{
-                  flexDirection: 'row',
-                }}
-              >
-                <Text>👍</Text>
-                <Text>💬</Text>
-                <Text>✈</Text>
-              </View>
-              <Text>🏴</Text>
+              <Text>👍</Text>
+              <Text>💬</Text>
+              <Text>✈</Text>
             </View>
-            <View>
-              <Text style={{ fontWeight: 'bold' }}>{item.likes} likes</Text>
-              <View style={{ flexDirection: 'row' }}>
-                <Text style={{ fontWeight: 'bold' }}>{item.username} </Text>
-                <Text>{item.caption}</Text>
-              </View>
-              <Text style={{ color: 'grey' }}>{item.comments} comments</Text>
-            </View>
+            <Text>🏴</Text>
           </View>
-        )}
-      />
-    </ScrollView>
+          <View>
+            <Text style={{ fontWeight: 'bold' }}>{item.likes} likes</Text>
+            <View style={{ flexDirection: 'row' }}>
+              <Text style={{ fontWeight: 'bold' }}>{item.username} </Text>
+              <Text>{item.caption}</Text>
+            </View>
+            <Text style={{ color: 'grey' }}>{item.comments} comments</Text>
+          </View>
+        </View>
+      )}
+    />
   );
 };
 
